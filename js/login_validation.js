041c209788d44a589f9ff2578e7ff811fd812739
danielaanabel <danielaanabel2011@gmail.com>
2021-08-12 09:12:39 -0300

    correccion

--- js/login_validation.js
+++ js/login_validation.js
@@ -8,13 +8,11 @@ var contraseña = document.getElementById("contraseña").value;
 
 var medico_correo = "denisaBregoli@gmail.com";
 
-if (correo != paciente_correo || correo != medico_correo || contraseña != 123) {
+if (correo != medico_correo || contraseña != 123) {
     document.getElementById("confirmacion").innerHTML = "Datos ingresados erróneamente";
 }
-if (correo === paciente_correo && contraseña == 123) {
-    window.location.href = "cuenta_paciente.html";
-}
 if (correo === medico_correo && contraseña == 123) {
-     window.location.href = "turnos_medico.html";
+    window.open("turnos_medico.html");
+  
     }
-} 
+}
